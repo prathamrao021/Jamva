1b147f72edc1e027b5715183651bc0a9022fb94e
prathamrao021 <prathamrao021@gmail.com>
2023-01-01 17:19:33 +0530

    timer

--- src/Register.js
+++ src/Register.js
@@ -4,6 +4,7 @@ import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
 import {useState} from 'react';
 import { Navigate, useNavigate, useParams } from "react-router-dom";
+import jamva from './Jamva.png';
 
 function Register() {
     let navigate = useNavigate();
@@ -13,7 +14,10 @@ function Register() {
     const [phone, setPhone] = useState("");
     const [message, setMessage] = useState("");
     const [otp, setOTP] = useState("");
-    
+    let [mins, setMins] = useState(1);
+    let [secs, setSecs] = useState(5);
+    let myInterval;
+
     const Sub = async (e) =>{ 
 
         e.preventDefault();
@@ -38,25 +42,63 @@ function Register() {
     };
 
     const getOTP = async (e) => {
-        e.preventDefault();
-        try {
-          let res = await fetch(`http://127.0.0.1:8000/userlogin/${phone}/${name}`, {
-            method: "GET",
-          });
-          let resJson = await res.json();
-         userId = resJson.id;
-          if (res.status === 200) {
-            setMessage("User created successfully");
-          } else {
-            setMessage("Some error occured");
-          }
-        } catch (err) {
-          console.log(err);
+        // e.preventDefault();
+        // try {
+        //   let res = await fetch(`http://127.0.0.1:8000/userlogin/${phone}/${name}`, {
+        //     method: "GET",
+        //   });
+        //   let resJson = await res.json();
+        //  userId = resJson.id;
+        //   if (res.status === 200) {
+        //     setMessage("User created successfully");
+        //   } else {
+        //     setMessage("Some error occured");
+        //   }
+        // } catch (err) {
+        //   console.log(err);
+        // }
+        
+        myInterval = setInterval(()=>{
+          stopwatch()
+        },1000
+        );
+    };
+
+    const stopwatch = () =>{
+      console.log(mins,secs);
+
+      // document.getElementbyId('getotp').innerHTML="mins:secs"
+
+      if(mins>=0)
+      { 
+        if(secs>=0)
+        {
+          setSecs((secs)=>(secs-1));
         }
+        else
+        {
+          console.log("ghjk")
+          setSecs(()=>60);
+          setMins((mins)=>(mins-1));
+        }
+      }
+      if(mins<0)
+      {
+        if(secs > 0)
+        {
+          setSecs((secs)=>(secs-1));;
+        }
+        if(secs===0)
+        {
+          clearInterval(myInterval);
+          console.log("Expired");
+        }
+      }
+      // setSecs((secs)=>(secs-1));
     };
 
   return (
-    <><img src={loginImage} ></img>
+    <><img className='registerimg' src={jamva} ></img>
     <div className="leftalign">
         <h1>Welcome!</h1>
         <Form name='formOne'>
@@ -72,7 +114,7 @@ function Register() {
             {/* <Form.Label>Password</Form.Label> */}
             <Form.Control type="text" placeholder="Phone Number" onChange={event => setPhone(event.target.value)} value={phone}/>
         </Form.Group>
-        <span onClick={getOTP} className='getotp'>GET OTP</span>
+        <span onClick={getOTP} className='getotp'>GET OTP {mins}:{secs}</span>
 
         <Form.Group className="mb-3 border1" controlId="formBasicPassword">
             {/* <Form.Label>Password</Form.Label> */}
